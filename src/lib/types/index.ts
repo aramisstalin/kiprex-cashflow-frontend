1717298bf3fee423ad46d88d78449f544094a24b
stalin <aramis.stalin@gmail.com>
2026-03-01 00:28:18 -0300

    Fixing type export

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -4,4 +4,4 @@ export { type UserBriefResponse, type UserResponse, type UserUpdate } from "./us
 export { type RoleCreate, type RoleResponse, type RoleUpdate, type PermissionResponse } from "./role";
 export { type SessionResponse } from "./session";
 export { type AuditLogResponse } from "./audit";
-export { type DashboardStatsResponse, type StatItem, type ActivityItem, type HealthResponse } from "./stats";
+export { type DashboardStatsResponse, type StatItem, type ActivityItem, type HealthResponse, type UserRoleData, type TopUserActivity, type AnalyticsResponse } from "./stats";
